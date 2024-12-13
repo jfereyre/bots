2f913f590eb885465fcd580b8a81af3a0205e58e
jerome fereyre <jfereyre@gmail.com>
2024-12-13 17:28:58 +0100

    Code refactoring

--- bot.py
+++ bot.py
@@ -10,33 +10,40 @@ from tkinter import Tk, Button
 from threading import Thread
 
 g_ex = None
-
-
-g_cells = CellMatrix(40,30)
-g_robotPosition=g_cells.get(0,0)
-g_targetPosition=g_cells.get(39,29)
+g_cells = None
 
 def BotSimulator():
-    global g_ex
+    global g_ex, g_cells
 
     while g_ex is None:
         pass
 
-    l_robot_path = AStar(g_robotPosition, g_targetPosition, g_cells, g_ex.max_col_index, g_ex.max_row_index)
+    while True:
+        g_ex.setRobotStartCell(0,0)
+        g_ex.setRobotEndCell(39,29)
 
-    for l_path_element in l_robot_path:
-        g_ex.setRobotPosition((l_path_element.col * 20) + 10, ( l_path_element.row * 20 ) + 10 )
-        sleep(0.2)
+        l_robot_path = AStar(g_ex.getRobotStartCell(), g_ex.getRobotEndCell(), g_cells, g_ex.max_col_index-1, g_ex.max_row_index-1)
+
+        for l_path_element in l_robot_path:
+            g_ex.setRobotPosition((l_path_element.col * 20) + 10, ( l_path_element.row * 20 ) + 10 )
+            sleep(0.2)
+
+def Reset():
+    global g_ex
+    g_ex.setRobotStartCell(0,0)
+    g_ex.setRobotEndCell(39,29)
+
+    g_ex.reInit()
 
 def UIStart():
-    global g_ex 
+    global g_ex, g_cells 
     
     root = Tk()
-
     g_ex = PathCanvas(800, 600, g_cells)
+
     root.geometry("800x650+300+300")
 
-    l_reset_button = Button(root, text="Reset", command=g_ex.reInit)
+    l_reset_button = Button(root, text="Start/Restart", command=g_ex.reInit)
 
     l_reset_button.pack(side='bottom')
 
@@ -52,14 +59,19 @@ def tagDetectionPositionChangeCallback(a_tag_id: int, a_box: Box):
 
         for l_x in range(l_topLeftCell.col, l_bottomRightCell.col):
             for l_y in range(l_topLeftCell.row, l_bottomRightCell.row):
-                g_ex.setCellOccupied(l_x, l_y)
+                g_cells.get(l_x, l_y).occupied()
 
 def main():
-    with open('config.yml', 'r') as l_config_file:
-        l_configuration = yaml.safe_load(l_config_file)
+    global g_ex, g_cells 
 
-        l_scanner_thread = CameraScanner(l_configuration['camera']['id'], tagDetectionPositionChangeCallback)
-        l_scanner_thread.start()
+    g_cells = CellMatrix(40,30)
+
+
+    #with open('config.yml', 'r') as l_config_file:
+    #    l_configuration = yaml.safe_load(l_config_file)
+#
+    #    l_scanner_thread = CameraScanner(l_configuration['camera']['id'], tagDetectionPositionChangeCallback)
+    #    l_scanner_thread.start()
     
     l_ui_thread = Thread(target=UIStart)
     l_ui_thread.start()
