320bff5b1e4621df10640c858a89ace660bcaf62
jerome fereyre <jfereyre@gmail.com>
2024-12-19 16:39:25 +0100

    Create a button to reset cells occupancy

--- botPathApp.py
+++ botPathApp.py
@@ -29,15 +29,22 @@ class BotPathApp(object):
         l_robot_thread = Thread(target=self.startBotSimulator)
         l_robot_thread.start()
 
+    def resetOccupancy(self):
+        self._m_game_board_ui.releaseOccupied()
+
     def start(self):
         global g_ex, g_cells 
         
         root = Tk()
 
-        root.geometry("800x650+300+300")
+        root.geometry("800x700+300+300")
         self._m_game_board_ui = GameBoardUI(800, 600, self._m_cells, self._m_robot)
 
         l_reset_button = Button(root, text="Start/Restart", command=self.resetBotSimulator)
+
+        l_reset_button.pack(side='bottom')
+
+        l_reset_button = Button(root, text="Reset Occupancy", command=self.resetOccupancy)
 
         l_reset_button.pack(side='bottom')
 
